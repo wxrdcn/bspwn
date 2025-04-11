49279a2c7f016e6d258d0328ead245bef2fdda85
wxrdcn <wxrdcn@localhost.htb>
2025-04-10 22:08:47 -0600

    x

--- install.sh
+++ install.sh
@@ -113,11 +113,11 @@ install_packages() {
         apg pwgen
 
         # Notifications & Appearance
-        dmenu dunst libnotify-bin lxappearance pavucontrol pamixer
+        dmenu dunst libnotify-bin lxappearance pavucontrol pamixer pasystray network-manager network-manager-gnome
 
         # Miscellaneous Utilities
         brightnessctl calc chrony ncal ranger redshift translate-shell zathura xcalib wmctrl
-      
+        
         # drive backup
         rclone
 
